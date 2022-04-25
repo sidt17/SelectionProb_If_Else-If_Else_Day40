3aef909df2512265f80e0b826170b22314ee772d
Siddhant <santoshshrivastava39@gmail.com>
2022-04-25 15:49:35 +0530

    Read Single digit number and write in word

---
+++ SingleDigitNumber.js
@@ -0,0 +1,45 @@
+//Uc-1 Read a single digit number and write the number in word
+
+let number = Math.floor(Math.random()*10)%10;
+console.log("Number : "+ number);
+
+
+if (number == 0) 
+{
+    console.log("In word- Zero");
+}
+else if (number == 1) 
+{
+    console.log("In word- One");
+}
+else if (number == 2) 
+{
+   console.log("In word- Two");
+} 
+else if (number == 3)
+{
+    console.log("In word- Three");
+} 
+else if (number == 4) 
+{
+    console.log("In word- Four");
+} 
+else if (number == 5) 
+{
+    console.log("In word- Five");
+}
+ else if (number == 6) 
+{
+    console.log("In word- Six");
+}
+ else if (number == 7)
+{
+    console.log("In word- Seven");
+} else if (number == 8)
+{
+    console.log("In word- Eight");
+} 
+else if (number == 9)
+{
+    console.log("In word- Nine");
+}
