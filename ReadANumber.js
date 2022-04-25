64bb62b60bd9602e131adfa24470b341f24d3736
Siddhant <santoshshrivastava39@gmail.com>
2022-04-25 23:51:28 +0530

    Read a number and display

---
+++ ReadANumber.js
@@ -0,0 +1,50 @@
+let power = Math.floor(Math.random()*10)%10;
+let number = 10**power;
+console.log("Number : "+number);
+
+if (number == 1) 
+{
+    console.log("One");
+}
+else if (number == 10)
+{
+    console.log("Ten");
+} 
+else if (number == 100)
+{
+    console.log("Hundred");
+} 
+else if (number == 1000) 
+{
+    console.log("Thousand");
+} 
+else if (number == 10000) 
+{
+    console.log("Ten thousand");
+}
+ else if (number == 100000)
+{
+    console.log("One Lakh");
+
+} 
+else if (number == 1000000)
+{
+    console.log("Ten Lakh");
+}
+else if (number == 10000000)
+{
+    console.log("Ten million");
+
+}
+else if (number == 100000000 )
+{
+    console.log("One hundred million");
+
+
+}
+else if (number == 1000000000 )
+{
+    console.log("One billion");
+
+}
+
